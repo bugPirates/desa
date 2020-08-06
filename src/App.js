6248f249247e3ca24962251c7124f9bca94d7b2a
Pirates <ican.notfound@gmail.com>
2020-08-06 08:10:11 +0800

    Navbar & Footer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 import './App.css';
+import Footer from './components/Footer';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Official Kawil",
+      headerLinks: [
+        { title: 'Beranda', path: '/' },
+        { title: 'Profil', path: '/profil'},
+        { title: 'Info', path: '/info'},
+        { title: 'Penduduk', path: '/penduduk'},
+        { title: 'Pembangunan', path: '/pembangunan'}
+      ],
+      beranda:{
+        title: 'Hai dunia',
+        subTitile: 'Project KP',
+        textL: 'Check out this'
+      },
+      profil:{
+        title: 'Profil desa'
+      },
+      info: {
+        title: 'Test'
+      },
+      penduduk: {
+        title: 'Test'
+      },
+      pembangunan: {
+        title: 'Test'
+      }     
+     }
+    }
+    render() {
+      return (
+        <Router>
+          <Container classname="p-0" fluid={true}>
+
+          <Navbar className="border-bottom" bg="transparent" expand="lg">
+            <Navbar.Brand>Kawil official</Navbar.Brand>
+
+            <Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
+            <Navbar.Collapse id="navbar-toggle">
+              <Nav className="ml-auto">
+                <Link className="nav-link" to="/">Beranda</Link>
+                <Link className="nav-link" to="/profil">Profil</Link>
+                <Link className="nav-link" to="/info">Info</Link>
+                <Link className="nav-link" to="/penduduk">Penduduk</Link>
+                <Link className="nav-link" to="/pembangunan">Pembangunan</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          {/* 
+          <Route path="/" exact render={() => <HomePage title={this.state.home.title} subTitle={this.state.home.subTitle} text={this.state.home.text} />} />
+          <Route path="/profil" render={() => <ProfilPage title={this.state.profil.title} />} />
+          <Route path="/info" render={() => <InfoPage title={this.state.info.title} />} />
+          <Route path="/penduduk" render={() => <PendudukPage title={this.state.penduduk.title} />} />
+          <Route path="/pembangunan" render={() => <PembangunanPage title={this.state.pembangunan.title} />} />
+          */}
+
+          <Footer />
+          </Container>
+        </Router>
+      );
+    }
+  }
+ 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Footer.css';
+
+function Footer() {
+    return( 
+            <footer>
+            <figure class="sm">
+                <p>Project KKP &#169; 2020, Universitas Hamzanwadi, Fakultas Tekhnik. &#8482;#stayAtHome</p>
+            </figure>
+            </footer>
+    );
+}
+export default Footer;
